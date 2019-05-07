a6ee89d6a6ed75a1f973f3eecae6302ee6b3c970
SanghoonJeong <berrycore@gmail.com>
2019-05-07 14:19:44 +0900

    Main Class 를 Usage_ServiceProvider 로 이름 변경

--- src/chapter1_creation_of_instance/Usage_ServiceProvider.java
+++ src/chapter1_creation_of_instance/Usage_ServiceProvider.java
@@ -1,9 +1,11 @@
+package chapter1_creation_of_instance;
+
 import chapter1_creation_of_instance.DefaultProvider;
 import chapter1_creation_of_instance.Provider;
 import chapter1_creation_of_instance.Service;
 import chapter1_creation_of_instance.Services;
 
-public class Main {
+public class Usage_ServiceProvider {
 
     public static void main(String[] args) {
 
