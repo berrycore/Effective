33a490ec2b9b5a614e437226b498bb40bac63d52
SanghoonJeong <berrycore@gmail.com>
2019-05-15 16:24:28 +0900

    hashCode 예제 작성 중

---
+++ src/chapter9_how_to_override_hashCode/PhoneNumber.java
@@ -0,0 +1,18 @@
+package chapter9_how_to_override_hashCode;
+
+public final class PhoneNumber {
+
+    private final short areaCode;
+    private final short prefix;
+    private final short lineNumber;
+
+    public PhoneNumber(int areaCode, int prefix, int lineNumber){
+
+    }
+
+
+    private static void rangeCheck(int arg, int max, String name) {
+        //TODO : 여기 할 차례
+    }
+
+}
