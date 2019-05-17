c9641e9489557fc06ce875a8462791801566dc86
SanghoonJeong <berrycore@gmail.com>
2019-05-17 15:32:06 +0900

    cloneable 인터페이스 구현 방법 및 유의사항

--- src/chapter11_how_to_override_clone/Usage_clone.java
+++ src/chapter11_how_to_override_clone/Usage_clone.java
@@ -19,12 +19,18 @@ public class Usage_clone {
      * 숙련된 프로그래머들은 clone 메소드를 전혀 오버라이드 하지 않고 호출하지도 않는다.
      *
      * 단점
-     * 1) final 키워드가 붙은 필드의 처리 곤란
-     * 2) deepCopy 시 clone() 재귀적 호출시 일일이 deepCopy 할 때 재귀호출 또는 반복문으로 처리, 코드 복잡해짐
+     *  1) final 키워드가 붙은 필드의 처리 곤란
+     *  2) deepCopy 시 clone() 재귀적 호출시 일일이 deepCopy 할 때 재귀호출 또는 반복문으로 처리, 코드 복잡해짐
      *
      *
      * 상속을 해주기 위한 클래스를 설계할 때 잘 동작하는 protected clone 메소드를 그 클래스에 두지 않는 다면
      * 서브 클래스에서 Cloneable 인터페이스를 제대로 구현할 수 없다는 것을 명심.
+     *
+     * 굳이 Cloneable 을 구현 한다면
+     *  1) 자신의 클래스를 반환타입으로 하는 public 메소드로 clone 를 오버라이드 한다.
+     *  2) super.clone() 를 호출한 후
+     *  3) 여타 필드들의 복제를 처리 한 다음
+     *  4) return 한다
      *
      * 그리하여, 복제 생성자 또는
      *      public Yum(Yum yum);
