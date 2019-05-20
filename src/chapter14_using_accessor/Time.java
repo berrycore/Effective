066556cc972d90342a294731e183d2c9a100515b
SanghoonJeong <berrycore@gmail.com>
2019-05-20 17:35:04 +0900

    member filed 에 public 으로 직접 접근하지 않도록 한다!!
    불변 필드를 외부로 노출하는 것은 그나마 다행이다..
    가변 필드는 절대로 외부에 노출하지 않도록 한다

---
+++ src/chapter14_using_accessor/Time.java
@@ -0,0 +1,21 @@
+package chapter14_using_accessor;
+
+public class Time {
+    private static final int HOURS_PER_DAY = 24;
+    private static final int MINUTES_PER_HOURS = 60;
+
+    public final int hour;
+    public final int minute;
+
+    public Time(int hour, int minute){
+        if (hour < 0 || hour >= HOURS_PER_DAY) {
+            throw new IllegalArgumentException("Hour : " + hour);
+        }
+        if (minute < 0 || minute >= MINUTES_PER_HOURS) {
+            throw new IllegalArgumentException("Min : " + minute);
+        }
+
+        this.hour = hour;
+        this.minute = minute;
+    }
+}
