761efbfb5100e795776c94a747b7d3c9dbd4ab99
SanghoonJeong <berrycore@gmail.com>
2019-05-22 17:27:33 +0900

    불변 클래스 몇개를 제공한다 (단, public static final 으로)

--- src/chapter15_minimize_variability/Complex.java
+++ src/chapter15_minimize_variability/Complex.java
@@ -7,6 +7,10 @@ public class Complex {
     private final double re;
     private final double im;
 
+    public static final Complex ZERO = new Complex(0,0);
+    public static final Complex ONE = new Complex(1,0);
+    public static final Complex I = new Complex(0,1);
+
     public Complex(double re, double im){
         this.re = re;
         this.im = im;
