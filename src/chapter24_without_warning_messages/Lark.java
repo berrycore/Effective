1edde81da4e1884601a0a96c47d424ea987f4ee0
SanghoonJeong <berrycore@gmail.com>
2019-06-04 14:41:38 +0900

    경고메시지를 무시하지 말고 최대한 없애도록 하라

---
+++ src/chapter24_without_warning_messages/Lark.java
@@ -0,0 +1,5 @@
+package chapter24_without_warning_messages;
+
+public class Lark {
+
+}
---
+++ src/chapter24_without_warning_messages/Usage_unchecked.java
@@ -0,0 +1,19 @@
+package chapter24_without_warning_messages;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Usage_unchecked {
+
+
+
+    public static void main(String[] args) {
+
+        //Set<Lark> exaltation = new HashSet();
+        //Set<Lark> exaltation = new HashSet<>();
+        Set<Lark> exaltation = new HashSet<Lark>();
+
+    }
+}
