dea9b4d758933fcf6e47106af7fc63cb98a904bc
SanghoonJeong <berrycore@gmail.com>
2019-06-25 17:38:07 +0900

    using annotation

---
+++ src/chapter35_use_annotation_instead_of_naming_pattern/Test.java
@@ -0,0 +1,11 @@
+package chapter35_use_annotation_instead_of_naming_pattern;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.ANNOTATION_TYPE.METHOD)
+public @interface Test {
+}
